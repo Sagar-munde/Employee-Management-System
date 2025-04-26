360ff168e38fbfedf8b3a1517f443d273647ad4a
Sagar-munde <103348620+Sagar-munde@users.noreply.github.com>
2025-04-26 13:02:48 +0530

    Update AdminDashboard.jsx

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -5,7 +5,7 @@ import AllTask from '../other/AllTask'
 
 const AdminDashboard = (props) => {
   return (
-    <div className='h-screen w-full p-10'>
+    <div className="min-h-screen w-full p-4 sm:p-10">
       <Header changeUser={props.changeUser}/>
       <CreateTask/>
       <AllTask/>
